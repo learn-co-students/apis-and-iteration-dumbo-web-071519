611eddd84372219bdb15a8918cccd4c7c647fa9b
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-07-16 15:53:34 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,15 +2,28 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def get_character_movie_URLs_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+  chosen_character = response_hash["results"].select do |character_hash|
+    character_hash["name"].downcase == character_name
+  end
+  chosen_character[0]["films"]
+end
+
+def collect_film_info(film_URL_array)
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
+  film_URL_array.map { |film_URL|
+    response_string = RestClient.get(film_URL)
+    response_hash = JSON.parse(response_string)
+    
+  }
+  # binding.pry
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
@@ -20,14 +33,24 @@ end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.map { |film|
+    p film["title"]
+  }
 end
 
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
+  films = get_character_movie_URLs_from_api (character)
+  film_info = collect_film_info(films)
+  print_movies(film_info)
 end
 
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+
+# # test run method
+# film_URLs_array = get_character_movie_URLs_from_api("luke skywalker")
+# film_info =  collect_film_info(film_URLs_array)
+# print_movies(film_info)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,17 @@
+require 'pry'
+
 def welcome
   # puts out a welcome message here!
+  puts " Welcome to the Star Wars!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a Character Name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  character = gets.strip
+  character.downcase
 end
+
+
+# welcome()
+# get_character_from_user()
